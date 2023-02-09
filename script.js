6b43b2c6399d9b985fc5a872e89f19d8c26a3965
Monir Ahmadyar <monirahmadyar123@gmail.com>
2023-02-09 18:41:49 +0430

    duplicate order merges successfully

--- script.js
+++ script.js
@@ -11,50 +11,69 @@ function add_row(search_value)
         
         if(tbody_length > 0)
         {
+            var check = Array();
             for (var i = 0; i < tbody_length; i++) {
                 var ID = tbody.rows[i].id;
                 var id_to_arr = ID.split('_');
-                // var cells = tbody.rows[i].getElementById('product_id_'+id_to_arr[2]);
-                // for (var ic=0,it=cells.length;ic<it;ic++) {
-                //     // alert the table cell contents
-                //     // you probably do not want to do this, but let's just make
-                //     // it SUPER-obvious  that it works :)
-                //     alert(cells[ic].innerHTML);
-                // }
-    
-                alert('product_id_'+id_to_arr[2]);
-    
-    
+                
+                var td_input_value = document.getElementById('product_id_'+id_to_arr[2]).value;
+
+                if (search_value == td_input_value) {
+                    var Quantity_td_value = document.getElementById('Quantity_id_'+id_to_arr[2]).value;
+                    var New_Quantity_Value = parseFloat(Quantity_td_value) + 1;
+                    document.getElementById('Quantity_id_'+id_to_arr[2]).value = New_Quantity_Value;
+                    check.push('true');
+                }
+                else
+                {
+                    check.push('false');
+                }
+                
             }
+
+            
+            
+            if(check.includes("true") == false)
+            {
+                var new_row_content='';
+                new_row_content += '<td>'+counter+'</td>';
+                new_row_content += '<td id="product_td_'+counter+'"><input type="text" value="'+search_value+'" id="product_id_'+counter+'" /></td>';
+                new_row_content += '<td><input type="text" value="1" id="Quantity_id_'+counter+'" /></td>';
+                new_row_content += '<td><input type="text" value="" id="Price_1_id_'+counter+'" /></td>';
+                new_row_content += '<td><input type="text" value="" id="Row_Total_id_'+counter+'" /></td>';
+        
+                new_row_content += '<td><button>X</button></td>';
+        
+                var tbody_length = tbody.rows.length;
+                var added_new_row = tbody.insertRow(tbody_length);
+                added_new_row.id = 'row_id_'+counter;
+        
+                added_new_row.innerHTML = new_row_content;
+            }
+            else
+            {
+
+            }
+
         }
         else
         {
-
+            var new_row_content='';
+            new_row_content += '<td>'+counter+'</td>';
+            new_row_content += '<td id="product_td_'+counter+'"><input type="text" value="'+search_value+'" id="product_id_'+counter+'" /></td>';
+            new_row_content += '<td><input type="text" value="1" id="Quantity_id_'+counter+'" /></td>';
+            new_row_content += '<td><input type="text" value="" id="Price_1_id_'+counter+'" /></td>';
+            new_row_content += '<td><input type="text" value="" id="Row_Total_id_'+counter+'" /></td>';
+     
+            new_row_content += '<td><button>X</button></td>';
+    
+            var tbody_length = tbody.rows.length;
+            var added_new_row = tbody.insertRow(tbody_length);
+            added_new_row.id = 'row_id_'+counter;
+    
+            added_new_row.innerHTML = new_row_content;
         }
 
-        
-
-
-
-
-        
-        var new_row_content='';
-        new_row_content += '<td>'+counter+'</td>';
-        new_row_content += '<td><input type="text" value="'+search_value+'" id="product_id_'+counter+'" /></td>';
-        new_row_content += '<td><input type="text" value="1" id="Quantity_id_'+counter+'" /></td>';
-        new_row_content += '<td><input type="text" value="" id="Price_1_id_'+counter+'" /></td>';
-        new_row_content += '<td><input type="text" value="" id="Row_Total_id_'+counter+'" /></td>';
- 
-        new_row_content += '<td><button>X</button></td>';
-
-       
-
-
-        var tbody_length = tbody.rows.length;
-        var added_new_row = tbody.insertRow(tbody_length);
-        added_new_row.id = 'row_id_'+counter;
-
-        added_new_row.innerHTML = new_row_content;
 
 
         document.getElementById("search_input").value ='';
