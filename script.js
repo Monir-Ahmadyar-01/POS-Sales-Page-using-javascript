6eb1aa0f8940ac5d60b568bbc0ff61be4a34d6cc
Monir Ahmadyar <monirahmadyar123@gmail.com>
2023-02-11 09:29:57 +0430

    up to date changes

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ function add_row(search_value)
                     var Quantity_td_value = document.getElementById('Quantity_id_'+id_to_arr[2]).value;
                     var New_Quantity_Value = parseFloat(Quantity_td_value) + 1;
                     document.getElementById('Quantity_id_'+id_to_arr[2]).value = New_Quantity_Value;
+
+                    calculation();
+
                     check.push('true');
                 }
                 else
@@ -36,13 +39,13 @@ function add_row(search_value)
             if(check.includes("true") == false)
             {
                 var new_row_content='';
-                new_row_content += '<td>'+counter+'</td>';
+                new_row_content += '<td id="counter_id_'+counter+'">'+counter+'</td>';
                 new_row_content += '<td id="product_td_'+counter+'"><input type="text" value="'+search_value+'" id="product_id_'+counter+'" /></td>';
-                new_row_content += '<td><input type="text" value="1" id="Quantity_id_'+counter+'" /></td>';
-                new_row_content += '<td><input type="text" value="" id="Price_1_id_'+counter+'" /></td>';
-                new_row_content += '<td><input type="text" value="" id="Row_Total_id_'+counter+'" /></td>';
+                new_row_content += '<td><input type="text" onkeyup="calculation()" value="1" id="Quantity_id_'+counter+'" /></td>';
+                new_row_content += '<td><input type="text" onkeyup="calculation()" value="" id="Price_1_id_'+counter+'" /></td>';
+                new_row_content += '<td><input type="text" value="" readonly id="Row_Total_id_'+counter+'" /></td>';
         
-                new_row_content += '<td><button>X</button></td>';
+                new_row_content += '<td><button onclick="Delete_Row(this)">X</button></td>';
         
                 var tbody_length = tbody.rows.length;
                 var added_new_row = tbody.insertRow(tbody_length);
@@ -59,13 +62,13 @@ function add_row(search_value)
         else
         {
             var new_row_content='';
-            new_row_content += '<td>'+counter+'</td>';
+            new_row_content += '<td id="counter_id_'+counter+'">'+counter+'</td>';
             new_row_content += '<td id="product_td_'+counter+'"><input type="text" value="'+search_value+'" id="product_id_'+counter+'" /></td>';
-            new_row_content += '<td><input type="text" value="1" id="Quantity_id_'+counter+'" /></td>';
-            new_row_content += '<td><input type="text" value="" id="Price_1_id_'+counter+'" /></td>';
+            new_row_content += '<td><input type="text" onkeyup="calculation()" value="1" id="Quantity_id_'+counter+'" /></td>';
+            new_row_content += '<td><input type="text" onkeyup="calculation()" value="" id="Price_1_id_'+counter+'" /></td>';
             new_row_content += '<td><input type="text" value="" id="Row_Total_id_'+counter+'" /></td>';
      
-            new_row_content += '<td><button>X</button></td>';
+            new_row_content += '<td><button id="delete_row_id_'+counter+'" onclick="Delete_Row(this)">X</button></td>';
     
             var tbody_length = tbody.rows.length;
             var added_new_row = tbody.insertRow(tbody_length);
@@ -82,7 +85,67 @@ function add_row(search_value)
 
 
         counter++;
+
+        set_counter();
+    }
+  
+}
+
+function set_counter()
+    {
+        var tbody_length = tbody.rows.length;
+        for (let index = 0; index < tbody_length; index++) {
+            var ID = tbody.rows[index].id;
+            var id_to_arr = ID.split('_');
+
+            document.getElementById('counter_id_'+id_to_arr[2]).innerHTML = index+1;
+
+            
+        }
+
+    }
+    
+function calculation()
+    {
+  
+        var tbody = document.getElementById('sales_tbody');
+        var tbody_length = tbody.rows.length;
+
+        var total_price = 0;
+
+        for (let index = 0; index < tbody_length; index++) {
+            var ID = tbody.rows[index].id;
+            var id_to_arr = ID.split('_');
+
+            // alert(id_to_arr[2]);
+            var row_quantity = parseFloat(document.getElementById('Quantity_id_'+id_to_arr[2]).value);
+            var row_price = parseFloat(document.getElementById('Price_1_id_'+id_to_arr[2]).value);
+
+            total_price += (row_quantity * row_price);
+
+            document.getElementById('Row_Total_id_'+id_to_arr[2]).value = (row_quantity * row_price).toFixed(2);
+            
+            
+        }
+        
+        document.getElementById('total_input').value = total_price.toFixed(2);
+
+        discount();
+        
     }
 
-    
-}
+    function discount()
+    {
+        var total = parseFloat(document.getElementById('total_input').value);
+        var discount_input = parseFloat(document.getElementById('discount_input').value);
+
+        var after_discount = total - ((discount_input * total)/100);
+        document.getElementById('final_price_input').value = after_discount.toFixed(2);
+
+    }
+
+    function Delete_Row(index) {
+        
+        document.getElementById('tbody').deleteRow(index.parentNode.parentNode.rowIndex);
+
+    }
